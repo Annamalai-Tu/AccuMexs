6b2ed801745c2fddd5abecfd2f5f7ecc2dba5eee
Annamalai <annamalai.raja@Testunity.com>
2024-12-11 16:04:06 +0530

    Updating Changes

--- src/test/java/framework/init/WebDriverInit.java
+++ src/test/java/framework/init/WebDriverInit.java
@@ -67,7 +67,10 @@ public class WebDriverInit extends Generics implements Configuration {
         //driver = new RemoteWebDriver(Configuration.getRemoteGridURL(), capabilities);
 
         DesiredCapabilities dc = new DesiredCapabilities();
-        URL url = new URL("http://localhost:4444/wd/hub");
+        String gridHost = System.getenv("SELENIUM_HOST") != null ? System.getenv("SELENIUM_HOST") : "localhost";
+        URL url = new URL("http://" + gridHost + ":4444/wd/hub");
+
+        //URL url = new URL("http://localhost:4444/wd/hub");
         dc.setBrowserName("chrome");
        // dc.setVersion("latest");
         driver = new RemoteWebDriver(url, dc);
