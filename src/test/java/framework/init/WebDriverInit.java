55dacf497844cbf7f234fcb592c2ff2e5089c07a
Annamalai <annamalai.raja@Testunity.com>
2024-12-11 12:26:33 +0530

    Updating Changes

--- src/test/java/framework/init/WebDriverInit.java
+++ src/test/java/framework/init/WebDriverInit.java
@@ -67,7 +67,7 @@ public class WebDriverInit extends Generics implements Configuration {
         //driver = new RemoteWebDriver(Configuration.getRemoteGridURL(), capabilities);\
         DesiredCapabilities dc = new DesiredCapabilities();
         dc.setBrowserName("chrome");
-        URL url = new URL("http://localhost:4444/wd/hub");
+        URL url = new URL("http://<selenium-server-ip>:4444/wd/hub");
         driver = new RemoteWebDriver(url , dc);
         implicitWaitOf(driver, IMPLICIT_WAIT);
         maximizeWindow(driver);
